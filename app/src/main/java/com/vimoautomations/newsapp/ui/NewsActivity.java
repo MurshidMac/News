b933ba6c75aedb1c05c83bf1274ef7ad328636d1
CALCEY\Murshid <murshid@calcey.com>
2021-06-08 22:46:34 +0530

    working changes

--- app/src/main/java/com/vimoautomations/newsapp/ui/NewsActivity.java
+++ app/src/main/java/com/vimoautomations/newsapp/ui/NewsActivity.java
@@ -3,7 +3,9 @@ package com.vimoautomations.newsapp.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.vimoautomations.newsapp.R;
 
 public class NewsActivity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class NewsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        Log.i("Item selected ID"," clicked item"+ bottomNavigationView.getSelectedItemId());
     }
 }
